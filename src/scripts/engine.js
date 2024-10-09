64375e318ef7502f0416c7a704cebbf7cc663dc9
vandorocha <vanderlandio.zr@gmail.com>
2024-10-09 19:51:13 -0300

    Finalizado estrutura e estilização

---
+++ src/scripts/engine.js
@@ -0,0 +1,18 @@
+const state = {
+    score:{
+        playerScore: 0,
+        computerScores: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSpriters:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    }
+}
+
+function init(){
+
+};
+
+init();
